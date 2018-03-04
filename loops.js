9aed4658dd8595a73729777ef28a4a374e3c3156
Marcus Jun <mjun@calstate.edu>
2018-03-04 18:20:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array){
-  var newArray=array
+  //var newArray=array
   for (let i=0; i<25; i++){
     if (i===1){
-      newArray.push("I am "+${i}+" strange loop.")
+      array.push("I am "+${i}+" strange loop.")
     }
     else{
-      newArray.push("I am "+${i}+" strange loops.")
+      array.push("I am "+${i}+" strange loops.")
     }
   }
-  return newArray
+  return array
 }
