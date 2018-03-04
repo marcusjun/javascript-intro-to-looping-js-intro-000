4b3480d49af2f965cdf546282f1166eb08a0e65a
Marcus Jun <mjun@calstate.edu>
2018-03-04 18:36:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,10 +18,11 @@ function whileLoop(n){
   return "done"
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array){
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
   do {
     array.shift()
   } while (array.length>0&&maybeTrue())
