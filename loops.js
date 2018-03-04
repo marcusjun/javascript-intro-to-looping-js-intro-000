22ef4625660635675988b9ba744a47517aa42541
Marcus Jun <mjun@calstate.edu>
2018-03-04 18:05:06 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array){
+  for (var i=0; i<24; i++){
+    if (i=1){
+      array.push("I am "+${i}+" strange loop.")
+    }
+    else{
+      array.push("I am "+${i}+" strange loop.")
+    }
+  }
+}
